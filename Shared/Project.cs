68018398e0a0ce18a3294cde2bb455d52be6f7b9
Louis <herren.louis@gmx.ch>
2023-10-14 10:11:26 +0200

    fixed layout stuff

--- Shared/Project.cs
+++ Shared/Project.cs
@@ -3,11 +3,17 @@ namespace scrum_board_tool.Shared
 {
     public class Project
     {
+        public Project()
+        {
+            Sprints = new HashSet<Sprint>();
+            Users = new HashSet<User>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
 
-        public virtual ICollection<Sprint> Sprints { get; set; } = null!;
-        public virtual ICollection<User> Users { get; set; } = null!;
+        public virtual ICollection<Sprint> Sprints { get; set; }
+        public virtual ICollection<User> Users { get; set; }
     }
 }
